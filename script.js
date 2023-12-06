5eac2745352ca451d3abb24b9cc0d50e45b71b84
aminafarah919 <aminafarah@cmail.carleton.ca>
2023-12-06 10:00:45 -0500

    updatee

--- script.js
+++ script.js
@@ -1,36 +1,233 @@
-const kitty = document.getElementById("kitty");
-  const bush = document.getElementById("bush");
+let currentInstruction = 1;
 
-  function jump() {
-    if (!kitty.classList.contains("jump")) {
-      kitty.classList.add("jump");
-      kitty.style.top = "-100px"; // Adjust the jump height as needed
+function showNextInstruction() {
+    const instructionContainer = document.getElementById("instructions-container");
 
-      setTimeout(function () {
-        kitty.style.top = "0";
-        kitty.classList.remove("jump");
-      }, 300);
+    // Hide the current step
+    const currentStep = document.getElementById(`step-${currentInstruction}`);
+    currentStep.style.display = "none";
+
+    // Display the next step or start game if all steps are completed
+    currentInstruction++;
+
+    const nextStep = document.getElementById(`step-${currentInstruction}`);
+    if (nextStep) {
+        nextStep.style.display = "block";
+    } else {
+        // If there is no next step, hide the "Next" button
+        document.getElementById("next-btn").style.display = "none";
+
+        // Show the "Now you can start!" section with the start button
+        document.getElementById("hellokitty").style.display = "block";
+        document.getElementById("start-btn").style.display = "block";
     }
-  }
+}
 
-  let isAlive = setInterval(function () {
-    // get current kitty Y position
-    let kittyTop = parseInt(window.getComputedStyle(kitty).getPropertyValue("top"));
 
-    // get current bush X position
-    let bushLeft = parseInt(
-      window.getComputedStyle(bush).getPropertyValue("left")
+function startAdventure() {
+    // Add functionality for starting the adventure
+    console.log("Adventure started!");
+}
+
+function restartGame() {
+    // Add functionality for restarting the game
+    console.log("Game restarted!");
+}
+
+
+
+let standImg = new Image();
+standImg.src = "./stand.png";
+
+let walkImg = new Image();
+walkImg.src = "./walk.png";
+
+let kitty = {
+    x: 50,
+    y: 170,
+    width: 130,
+    height: 140,
+    jumping: false,
+    jumpHeight: 200,
+    jumpCount: 0,
+    img: standImg // Set the initial image to stand.png
+};
+
+let bush = {
+    x: 800,
+    y: 220,
+    width: 130,
+    height: 110
+};
+
+let bushImg = new Image();
+bushImg.src = "./bush.png";
+
+let velocityX = -8;
+let velocityY = 0;
+let gravity = 0.2;
+
+let score = 0;
+let gameOver = false;
+
+let board;
+let context;
+
+// Background music
+let backgroundMusic = new Audio("./song.mp3");
+
+window.onload = function () {
+    board = document.getElementById("board");
+    board.width = 700;
+    board.height = 300;
+    context = board.getContext("2d");
+
+    document.addEventListener("keydown", jumpKitty);
+
+    // Load the kitty and bush images
+    kitty.img.onload = function () {
+        requestAnimationFrame(update);
+    };
+    bushImg.onload = function () {
+        requestAnimationFrame(update);
+    };
+
+    // Start the game with the stand image
+    kitty.img.src = standImg.src;
+    bushImg.src = "./bush.png";
+
+    // Start background music
+    backgroundMusic.play();
+};
+
+function startAdventure() {
+    document.getElementById("welcome-page").style.display = "none"; // Hide the welcome page
+    document.getElementById("game-container").style.display = "block"; // Show the game container
+    initGame(); // Initialize the game
+}
+
+function initGame() {
+    // Reset any game variables if needed
+    score = 0;
+    gameOver = false;
+
+    // Set up your game canvas and context
+    board = document.getElementById("board");
+    board.width = 700;
+    board.height = 300;
+    context = board.getContext("2d");
+
+    // Load the kitty and bush images
+    kitty.img.onload = function () {
+        requestAnimationFrame(update);
+    };
+    kitty.img.src = "./kittyrun.png";
+
+    bushImg.onload = function () {
+        requestAnimationFrame(update);
+    };
+    bushImg.src = "./bush.png";
+
+    // Add event listener for jumping
+    document.addEventListener("keydown", jumpKitty);
+
+    // Start the game loop
+    requestAnimationFrame(update);
+}
+
+function update() {
+    if (!gameOver) {
+        context.clearRect(0, 0, board.width, board.height);
+
+        // Draw Kitty
+        context.drawImage(kitty.img, kitty.x, kitty.y, kitty.width, kitty.height);
+
+        // Draw Bush
+        context.drawImage(bushImg, bush.x, bush.y, bush.width, bush.height);
+
+        // Update Kitty position
+        if (kitty.jumping) {
+            kitty.y -= velocityY;
+            velocityY -= gravity;
+
+            if (kitty.y >= 200) {
+                kitty.y = 200;
+                velocityY = 0;
+                kitty.jumping = false;
+            }
+        }
+
+        // Check for collision
+        if (detectCollision(kitty, bush)) {
+            gameOver = true;
+            showGameOver();
+        }
+
+        // Move Bush with varying speed
+        bush.x += velocityX;
+
+        // Check if Bush is out of bounds
+        if (bush.x + bush.width < 0) {
+            bush.x = board.width;
+            score++;
+            updateScore();
+
+            // Increase speed a little when the score reaches 50
+            if (score >= 50) {
+                velocityX = -10; // Adjust the speed as needed
+            }
+        }
+
+        requestAnimationFrame(update);
+    }
+}
+
+function jumpKitty(event) {
+    if (event.code === "Space" && !kitty.jumping && kitty.y === 200) {
+        kitty.jumping = true;
+        velocityY = 9; // Initial jump velocity
+
+        // Change the kitty image to "walk.png"
+        kitty.img.src = walkImg.src;
+    }
+}
+
+function detectCollision(rect1, rect2) {
+    return (
+        rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.y + rect1.height > rect2.y
     );
+}
 
-    // detect collision
-    if (bushLeft < 50 && bushLeft > 0 && kittyTop >= 140) {
-      // collision
-      alert("Game Over!");
-    }
-  }, 10);
+function showGameOver() {
+    document.getElementById("game-over").style.display = "block";
+    document.getElementById("final-score").innerText = `final score: ${score}`;
+    document.getElementById("restart-btn").style.display = "block";
 
-  document.addEventListener("keydown", function (event) {
-    if (event.code === "Space") {
-      jump();
-    }
-  });
+    // Stop background music
+    backgroundMusic.pause();
+}
+
+function updateScore() {
+    document.getElementById("score-value").innerText = score;
+}
+
+function restartGame() {
+    kitty.y = 200;
+    bush.x = 500;
+    velocityY = 0;
+    score = 0;
+    gameOver = false;
+
+    document.getElementById("game-over").style.display = "none";
+    document.getElementById("restart-btn").style.display = "none";
+    updateScore();
+
+    // Resume background music
+    backgroundMusic.currentTime = 0;
+    backgroundMusic.play();
+
+    update();
+}
